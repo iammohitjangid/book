31757f760dd75be1a8b161e697f523ef7047aec2
mohitjantino <mohit.j@antino.io>
2024-03-16 16:33:31 +0530

    done

--- src/pages/Admin/Home/AdminHome.js
+++ src/pages/Admin/Home/AdminHome.js
@@ -1,26 +1,10 @@
 import { message } from 'antd';
 import Hero from '../../../components/hero/Hero';
-import API from '../../../services/apiAxios';
 import React, { useEffect, useState } from 'react';
 import OrderCard from '../../../components/orderCard/OrderCard';
 import { ForEach } from '../../../components/ForEach';
 
 const AdminCart = () => {
-  const [orderData, setOrderData] = useState([]);
-  const fetchOrders = async () => {
-    try {
-      const response = await API.get('api/v1/order');
-      setOrderData(response?.data?.data?.orders);
-    } catch (error) {
-      message.destroy();
-      message.error(error?.response?.data?.message);
-    }
-  };
-
-  useEffect(() => {
-    fetchOrders();
-  }, []);
-
   return (
     <div>
       <Hero
@@ -28,10 +12,11 @@ const AdminCart = () => {
         description="Check out new Orders from Users"
       />
       <div className="flex flex-row flex-wrap p-8">
-        <ForEach
+        {/* <ForEach
           of={orderData}
           render={(order, index) => <OrderCard key={index} order={order} />}
-        />
+        /> */}
+        <OrderCard />
       </div>
     </div>
   );
